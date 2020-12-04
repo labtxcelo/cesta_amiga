3e963b1f1523e6115caaae43f802a0e797dd44d8
Mozart Sousa <mozartsousa@frwk.com.br>
2020-12-04 12:40:42 -0300

    SolicitaçãoController criada

---
+++ lib/screens/solicitante_screen/solicitante_controller.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'solicitante_controller.g.dart';
+
+class SolicitanteController = _SolicitanteControllerBase
+    with _$SolicitanteController;
+
+abstract class _SolicitanteControllerBase with Store {
+} //end solicitanteController
